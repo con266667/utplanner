d4278661c328a5ab2428e952a08d4639a0cc57ff
Connor Wilson <cnwilson29@gmail.com>
2023-06-26 12:37:11 -0400

    functions

--- functions/api/greetings.js
+++ functions/api/greetings.js
@@ -1,10 +1,10 @@
 // GET requests to /filename would return "Hello, world!"
-export const onRequestGet = () => {
+export function onRequestGet(){
   return new Response("Hello, world!")
 }
   
 // POST requests to /filename with a JSON-encoded body would return "Hello, <name>!"
-export const onRequestPost = async ({ request }) => {
+export async function onRequestPost({ request }) {
   const { name } = await request.json()
   return new Response(`Hello, ${name}!`)
 }
