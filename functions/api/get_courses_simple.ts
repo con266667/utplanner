764b803fac652ae925870aec944bb7f0c5abff46
Connor Wilson <cnwilson29@gmail.com>
2023-11-14 02:00:01 -0500

    Refactor course search functionality

--- functions/api/get_courses_simple.ts
+++ functions/api/get_courses_simple.ts
@@ -36,44 +36,26 @@ export const onRequestPost = async (context) => {
     body.pageSize = 10;
   }
 
+  if (body.search) {
+    body.courseCodeAndTitleProps = {
+      "courseCode": "",
+      "courseTitle": body.search,
+      "courseSectionCode": "",
+      "searchCourseDescription": true
+    }
+    body.search = undefined;
+  }
+
 
   var response = await fetch("https://api.easi.utoronto.ca/ttb/getPageableCourses", {
       method: 'POST',
       headers: myHeaders,
-      body: JSON.stringify(await context.request.json())
+      body: JSON.stringify(body),
   })
   var data = await response.json();
   data = data.payload.pageableCourse.courses;
 
   var importantData: any[] = [];
-
-  // "sections": [
-  //   {
-  //       "name": "TUT0105",
-  //       "type": "Tutorial",
-  //       "teachMethod": "TUT",
-  //       "sectionNumber": "0105",
-  //       "meetingTimes": [
-  //           {
-  //               "start": {
-  //                   "day": 4,
-  //                   "millisofday": 64800000
-  //               },
-  //               "end": {
-  //                   "day": 4,
-  //                   "millisofday": 68400000
-  //               },
-  //               "building": {
-  //                   "buildingCode": "BA",
-  //                   "buildingRoomNumber": "",
-  //                   "buildingRoomSuffix": "",
-  //                   "buildingUrl": "https://map.utoronto.ca/?id=1809#!m/494470",
-  //                   "buildingName": null
-  //               },
-  //               "sessionCode": "20239",
-  //               "repetition": "WEEKLY",
-  //               "repetitionTime": "ONCE_A_WEEK"
-  //           },
 
   for (var i = 0; i < data.length; i++) {
     importantData.push({
@@ -86,7 +68,7 @@ export const onRequestPost = async (context) => {
             return {
               "start": meetingTime.start,
               "end": meetingTime.end,
-              "building": meetingTime.building,
+              "building": meetingTime.building.buildingCode,
               "repetition": meetingTime.repetition,
               "repetitionTime": meetingTime.repetitionTime
             }
